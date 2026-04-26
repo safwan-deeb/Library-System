7238cf46e7956f1f211ddca47e9afae2e6667807
safwan alhmedy <s1a2f3w4a5n6@gmail.com>
2026-04-26 23:48:48 +0300

    Add project files.

---
+++ Library System/Properties/IBorrowable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_System.Properties
+{
+    public interface IBorrowable
+    {
+        void Borrow();
+        void Return();
+    }
+}
