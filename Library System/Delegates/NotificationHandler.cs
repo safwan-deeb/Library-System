164bfab4e4ce8b28bd8cd38af6b1531bf3321db6
safwan alhmedy <s1a2f3w4a5n6@gmail.com>
2026-05-12 19:41:15 +0300

    لقد تم أضافة هذه الملفات بعد التعديل عليها لان الملفات القديما كانت ناقصا

---
+++ Library System/Delegates/NotificationHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Library_System.Delegates
+{
+    public delegate void NotificationHandler(string message);
+}
